613204f91ff1fc6d2de6e6e460939d79859c60d4
Dafainic20 <mdafaidha@gmail.com>
2023-11-27 14:37:59 +0700

    fc

---
+++ resources/views/bukubudi.blade.php
@@ -0,0 +1,31 @@
+
+Catatan Hati Ibu
+(Nunu Karlina)
+
+Penulis: Poppy Yuditya
+Isi: Bookpaper, B/W, 154 halaman.
+Penerbit: Pustaka Nabawiyyah.
+Harga: @Rp.55.000
+Ini tentang rasa...
+Rasa seorang Ibu, yang berasal dari hati terdalam.
+Karena hati yang belajar,
+Selalu ingin mencari kebenaran.
+Dan kebenaran hakiki,
+Hanya ada pada Islam,
+Agama yang sempurna.
+
+ 
+
+Begitulah buku Catatan Hati Ibu ini ditulis; dari rasa, dari hati, dari pencarian, dari kesadaran, dan dari proses pembelajaran.
+
+Penulis terusik saat menghadapi kondisi saat "kebenaran" biasanya jadi hadir karena  trend. Yang dianggap kebenaran adalah yang di-share paling banyak, paling sesuai keinginan dan pemikiran diri. Mana yang tak cocok dan sulit dijalankan menjadi tertolak. Trend "mengalahkan" kebenaran yang sesungguhnya.
+
+Buku ini sejatinya adalah upaya untuk mengumpulkan berbagai hikmah yang terserak dari berbagai tulisan pada sebuah rubrik di www.parentingnabawiyyah.com, Selama beberapa tahun. Disusun dalam 3 bab. Pertama, Gelisahmu, Bunda. Kedua, Because It's My Happiness. Dan yang terakhir, Anakku Sayang. Mengajak kita untuk ikut merenung dan merasa.
+
+Selamat membaca,
+Selamat merenungi,
+Selamat berkontemplasi,
+Selamat merasai...
+
+Info dan pemesanan via WA ke 087880355991
+IMG-20171115-WA0005
